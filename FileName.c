a6c2c8df4739ab33525ba214a162b18f4587d83b
Amaurymrt78 <151008865+Amaurymrt78@users.noreply.github.com>
2023-11-23 10:30:24 +0100

    Add files via upload

---
+++ FileName.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    char nom[50];
+    int age;
+    char sexe;
+
+    printf("Bonjour, comment vous name ?");
+    printf("Bonjour %s !\n", nom);
+
+    printf("Quel est votre age ?");
+    printf("vous avez %d !\n", age);
+
+    printf("Quel est votre sexe ?");
+    printf("vous avez %c !\n", sexe);
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
