23eb9a03f8a79fd8effb2fa11da4cd6f1ff70504
Amaurymrt78 <151008865+Amaurymrt78@users.noreply.github.com>
2023-11-23 11:32:23 +0100

    Add files via upload

--- FileName.c
+++ FileName.c
@@ -1,22 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS 
 #include <stdio.h>
 
 int main() {
     char nom[50];
     int age;
-    char sexe;
 
-    printf("Bonjour, comment vous name ?");
+    printf("Bonjour, comment vous appelez-vous ?");
+    scanf("%s", &nom);
     printf("Bonjour %s !\n", nom);
 
     printf("Quel est votre age ?");
+    scanf("%d", &age);
     printf("vous avez %d !\n", age);
-
-    printf("Quel est votre sexe ?");
-    printf("vous avez %c !\n", sexe);
-
-
+    sexecheck();
     return 0;
 }
+
+int sexecheck() {
+    char sexe;
+    printf("Entrez votre sexe (m ou f ou nb) : ");
+    scanf(" %c", &sexe);
+    if (sexe == 'm') {
+        printf("Vous etes un homme.\n");
+    }
+    else if (sexe == 'f') {
+        printf("Vous etes une femme.\n");
+    }
+    else if (sexe == 'n') {
+        printf("Vous etes non binaire");
+    }
+    return 0;
+}
+
+
 
 
 
